c6221e8677aa3bb109c690895eabc2c8ba99d1de
Gustavo Aguiar <gugahoa@gmail.com>
2017-06-03 07:09:56 -0300

    refactor: use perror instead of custom error msg

--- server.c
+++ server.c
@@ -22,17 +22,7 @@ int create_socket(uint64_t port) {
 	// 0 is the default protocol communcation
 	sock = socket(AF_INET, SOCK_STREAM, 0);
 	if (sock < 0) {
-		printf("Error creating socket: ", errno);
-
-		// Relevants errno
-		if (errno == EPROTOTYPE) {
-			printf("socket type not supported by protocol\n");
-		} else if (errno == EPROTONOSUPPORT) {
-			printf("protocol not supported\n");
-		} else {
-			printf("errno = %d\n", errno);
-		}
-
+		perror("Error creating socket");
 		return -1;
 	}
 
@@ -42,17 +32,7 @@ int create_socket(uint64_t port) {
 
 	// http://pubs.opengroup.org/onlinepubs/7908799/xns/bind.html
 	if (bind(sock, (struct sockaddr *)&name, sizeof(name)) < 0) {
-		printf("Error binding socket: ");
-
-		// Relevants errno
-		if (errno == EADDRINUSE) {
-			printf("specified address (%d) is in use\n", INADDR_ANY);
-		} else if (errno == EAFNOSUPPORT) {
-			printf("address not supported by address family\n");
-		} else {
-			printf("errno = %d\n", errno);
-		}
-
+		perror("Error binding socket");
 		return -1;
 	}
 
@@ -62,19 +42,7 @@ int create_socket(uint64_t port) {
 int listen_and_serve(int socket_fd) {
 	// http://pubs.opengroup.org/onlinepubs/7908799/xns/listen.html
 	if (listen(socket_fd, 12) < 0) {
-		printf("Error listening on socket: ");
-		if (errno == EBADF) {
-			printf("socket argument is not valid file descriptor\n");
-		} else if (errno == EDESTADDRREQ) {
-			printf("socket not bound\n");
-		} else if (errno == EINVAL) {
-			printf("socket already listening\n");
-		} else if (errno == ENOTSOCK) {
-			printf("socket argument is not a socket\n");
-		} else {
-			printf("errno = %d\n", errno);
-		}
-
+		perror("Error listening on socket");
 		return -1;
 	}
 
@@ -91,16 +59,7 @@ int listen_and_serve(int socket_fd) {
 		read_set = active_set;
 		// http://pubs.opengroup.org/onlinepubs/007908799/xsh/select.html
 		if (select(FD_SETSIZE, &read_set, NULL, NULL, NULL) < 0) {
-			printf("Error multiplexing socket connections: ");
-
-			if (errno == EBADF) {
-				printf("one or more not valid open socket file descriptor in set\n");
-			} else if (errno == EINVAL) {
-				printf("lots of possible errors, look for EINVAL in the doc\n");
-			} else {
-				printf("errno = %d\n", errno);
-			}
-
+			perror("Error multiplexing socket connections");
 			return -1;
 		}
 
@@ -120,16 +79,7 @@ int listen_and_serve(int socket_fd) {
 						&client_name_size);
 
 				if (new_connection < 0) {
-					printf("Error receiving connection: ");
-
-					if (errno == ECONNABORTED) {
-						printf("connection was aboted\n");
-					} else if (errno == EINVAL) {
-						printf("socket is not accepting new connection\n");
-					} else {
-						printf("errno = %d\n", errno);
-					}
-
+					perror("Error receiving connection");
 					return -1;
 				}
 
@@ -145,17 +95,7 @@ int listen_and_serve(int socket_fd) {
 				// http://pubs.opengroup.org/onlinepubs/7908799/xns/recv.html
 				nbytes = recv(i, buffer, 255, 0);
 				if (nbytes < 0) {
-					printf("Error reading from socket: ");
-					
-					if (errno == ECONNRESET) {
-						printf("connection closed by peer\n");
-					} else if (errno == EINTR) {
-						printf("recv was interrupted by signal\n");
-					} else if (errno == ETIMEDOUT) {
-						printf("connection timed out\n");
-					} else {
-						printf("errno = %d\n", errno);
-					}
+					perror("Error reading from socket");
 				} else if (nbytes > 0) {
 					buffer[nbytes - 1] = '\0';
 					printf("Received message of size %d: `%s`\n", nbytes, buffer);
