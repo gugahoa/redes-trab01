fd9153278a178f67d40f794c018ec20a416bd956
Gustavo Aguiar <gugahoa@gmail.com>
2017-06-03 08:11:30 -0300

    example of 2 threads sending info on diff interval

--- client/main.c
+++ client/main.c
@@ -4,10 +4,10 @@
 
 int main(int argc, char* argv[]) {
 	int sock = connect_host("localhost", 8080);
-	for (int i = 0; i < 100; ++i) {
-		send_random(sock);
-	}
+	send_random_interval(sock, 500000000);
+	send_random_interval(sock, 250000000);
 
+	sleep(2);
 	close(sock);
 	return 0;
 }
